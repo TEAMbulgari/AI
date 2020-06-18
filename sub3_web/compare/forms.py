f1a427af7276cbb1884760601193ee7c2bc3479a
godhpjh <godhpjh123@naver.com>
2020-06-18 13:41:20 +0900

    delete all previous AI

--- sub3_web/compare/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UploadimageForm(forms.Form):
-    file = forms.FileField()
-    
--- sub3_web/compare/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    photo = models.ImageField(blank=True)
-
-    
